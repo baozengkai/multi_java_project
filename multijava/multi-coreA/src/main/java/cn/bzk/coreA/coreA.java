358c46ed29a6335daeceb8d1371048e3e5d67759
bao.zengkai <bao.zengkai@eisoo.com>
2018-04-17 23:56:17 +0800

    鲍增凯:多Maven项目实战；完成了几个元素的学习，比如modules等

---
+++ multijava/multi-coreA/src/main/java/cn/bzk/coreA/coreA.java
@@ -0,0 +1,9 @@
+package cn.bzk.coreA;
+
+public class coreA {
+    public static void main(String[] args)
+    {
+        System.out.print("I am coreA");
+    }
+
+}
---
+++ multijava/multi-coreB/src/main/java/cn/bzk/coreB/coreB.java
@@ -0,0 +1,8 @@
+package cn.bzk.coreB;
+
+public class coreB {
+    public static void main(String[] args)
+    {
+        System.out.print("I am coreA");
+    }
+}
